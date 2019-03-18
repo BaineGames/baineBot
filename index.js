8b0eab9d63a679284a5e8456baecab5c0df2ea8d
Noah Ross <BaineGames@users.noreply.github.com>
2019-03-18 16:53:56 -0700

    Update index.js
    updated to make it instant

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const client = new Discord.Client({
   autoReconnect: true
 });
 const config = require("./config.json");
-const allowedChannelIds = ["456117300446232578", "458217769515876352", "519915583429541918", "456109329456562187", "457097244852879360"]; //519915583429541918 is baines test channel on his server
-
+const allowedChannelIds = ["456117300446232578", "553550313533997057", "553257340108013568", "458217769515876352", "519915583429541918", "456109329456562187"]; //519915583429541918 is baines test channel on his server
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -12,18 +11,29 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
   if (allowedChannelIds.indexOf(message.channel.id) != -1) {
-    setTimeout(function () {
-      if (message) {
-        let isVIP = message.channel.permissionsFor(message.author).has('MANAGE_MESSAGES');
+    let isVIP = message.member.hasPermission("MANAGE_MESSAGES");
         if (message.attachments.size == 0 && message.embeds.length == 0 && !isVIP) {
           message.delete()
             .then(msg => console.log(`Deleted message from ${msg.author.username}`))
             .catch(console.error);
         }
-      }
-    }, 5000);
   }
 });
 
 client.on("error", (e) => console.error(e));
 client.login(config.token);
+
+
+    // setTimeout(function () {
+    //   if (message) {
+    //     console.log("POST message attachments:", message.attachments);
+    //     console.log("POST message embeds:", message.embeds);
+    //     // let isVIP = message.channel.permissionsFor(message.author).has('MANAGE_MESSAGES');
+    //     // let isVIP = message.member.hasPermission("MANAGE_MESSAGES");
+    //     // if (message.attachments.size == 0 && message.embeds.length == 0 && !isVIP) {
+    //     //   message.delete()
+    //     //     .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+    //     //     .catch(console.error);
+    //     // }
+    //   }
+    // }, 5000);
